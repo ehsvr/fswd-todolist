53f44685942b614e63c1da28cec20eecbe9c280d
ehsvr <xehsvr@gmail.com>
2024-08-08 21:29:56 -0400

    fe fixes

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -2,12 +2,21 @@ import $ from 'jquery';
 import {
   indexTasks,
   postTask,
+  getAllTasks,
+  deleteOneTask,
+  markTasksAsCompleted,
+  markTaskAsActive
 } from "./requests.js";
+$(document).ready(function() {
+    console.log("Document Ready");
 indexTasks(function (response) {
+    console.log("All tasks: ", response);
+    
   var htmlString = response.tasks.map(function(task) {
     return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'> \
       " + task.content + "\
       </div>";
   });
   $("#tasks").html(htmlString);
+});
 });
--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -27,3 +27,80 @@ export var postTask = function (content, successCB, errorCB) {
   }
   $.ajax(request);
 };
+export function Request() {
+  this.type = '';
+  this.url = '';
+  this.data = {};
+  this.dataType = 'json';
+  this.success = function (response) {};
+  this.error = function (response) {};
+}
+
+//------------------- Get all Tasks ------------------
+
+export function getAllTasks(successCB, errorCB) {
+  var newRequest = new Request();
+  newRequest['type'] = 'GET';
+  newRequest['url'] = 'api/tasks?api_key=1';
+  newRequest['success'] = function (response) {
+    return successCB(response);
+  };
+  newRequest['error'] = function (request, errorMsg) {
+    return errorCB(request, errorMsg);
+  };
+
+  $.ajax(newRequest);
+}
+
+//---------------- Delete a Task by ID ----------------
+
+export function deleteOneTask(id, successCB, errorCB) {
+  var newRequest = new Request();
+  newRequest['type'] = 'DELETE';
+  newRequest['url'] = 'api/tasks/' + id + 'api_key=1'
+  newRequest['xhrFields'] = { withCredentials: true };
+  newRequest['success'] = function (response) {
+    console.log(response);
+
+    return successCB(response);
+  };
+  newRequest['error'] = function (request, errorMsg) {
+    return errorCB(request, errorMsg);
+  };
+
+  $.ajax(newRequest);
+}
+
+//--------------- Mark a Task as Completed by ID --------------
+
+export function markTaskAsComplete(id, successCB, errorCB) {
+  var newRequest = new Request();
+  newRequest['type'] = 'PUT';
+  newRequest['url'] = 'api/tasks/' + id + 'api_key=1' + '/mark_complete';
+  newRequest['xhrFields'] = { withCredentials: true };
+  newRequest['success'] = function (response) {
+    return successCB(response);
+  };
+  newRequest['error'] = function (request, errorMsg) {
+    return errorCB(request, errorMsg);
+  };
+
+  $.ajax(newRequest);
+}
+
+//--------------- Mark a Task as Active by ID --------------
+
+export function markTaskAsActive(id, successCB, errorCB) {
+  var newRequest = new Request();
+  newRequest['type'] = 'PUT';
+  newRequest['url'] = 'api/tasks/' + id + 'api_key=1' + '/mark_active';
+  newRequest['xhrFields'] = { withCredentials: true };
+  newRequest['success'] = function (response) {
+    return successCB(response);
+  };
+  newRequest['error'] = function (request, errorMsg) {
+    return errorCB(request, errorMsg);
+  };
+
+  $.ajax(newRequest);
+}
