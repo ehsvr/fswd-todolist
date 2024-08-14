ab8facafa3c1e40ce2f12c09b61a2c73ac981e48
ehsvr <xehsvr@gmail.com>
2024-08-14 12:12:22 -0400

    new commit

---
+++ app/javascript/src/requests.js
@@ -0,0 +1,76 @@
+import $ from 'jquery';
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
+export var indexTasks = function (successCB, errorCB) {
+  var request = {
+    type: 'GET',
+    url: 'api/tasks?api_key=1',
+    success: successCB,
+    error: errorCB
+  }
+  $.ajax(request);
+};
+
+export var postTask = function (content, successCB, errorCB) {
+  var request = {
+    type: 'POST',
+    url: 'api/tasks?api_key=1',
+    data: {
+      task: {
+        content: content
+      }
+    },
+    success: successCB,
+    error: errorCB
+  }
+  $.ajax(request);
+};
+
+export function request() {
+  this.type = '';
+  this.url = '';
+  this.data = {};
+  this.dataType = 'json';
+  this.success = function (response) {};
+  this.error = function (response) {};
+}
+
+//---------------- Delete a Task by ID ----------------
+
+export function deleteOneTask(id, successCB, errorCB) {
+  var request = {
+  type: 'DELETE',
+  url: 'api/tasks/' + id + '?api_key=1',
+  success: successCB,
+  error: errorCB
+  }
+  $.ajax(request);
+};
+
+//--------------- Mark a Task as Completed by ID --------------
+
+export function markTaskAsComplete(id, successCB, errorCB) {
+  var request = {
+  type: 'PUT',
+  url: 'api/tasks/' + id + '?api_key=1' + '/mark_complete',
+  success: successCB,
+  error: errorCB
+  }
+  $.ajax(request);
+};
+
+//--------------- Mark a Task as Active by ID --------------
+
+export function markTaskAsActive(id, successCB, errorCB) {
+  var request = {
+  type: 'PUT',
+  url: 'api/tasks/' + id + '?api_key=1' + '/mark_active',
+  success: successCB,
+  error: errorCB
+  }
+  $.ajax(request);
+};
